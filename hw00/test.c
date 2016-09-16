8582d37858068475abbe1b644b757141dc69660c
Bhanu <bhanujain88@gmail.com>
2016-09-15 22:20:09 -0400

    adding getcontext to checkpoint

--- hw00/test.c
+++ hw00/test.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <ucontext.h>
 
 int main(void) {
-	char * name = "bhanu";
-	int a = 1;
-	name+=1;
-	int * b = &a;
-	(*b)++;
-	printf("%s\n", name);
-	printf("%d\n", (*b));
-  // printf("Hello World!\n");
-  // return 0; 
-  }
+	//	char * name = "bhanu";
+	//	int a = 1;
+	//	name+=1;
+	//	int * b = &a;
+	//	(*b)++;
+	//	printf("%s\n", name);
+	//	printf("%d\n", (*b));
+	//  // printf("Hello World!\n");
+	//  // return 0;
+
+	ucontext_t p_context;
+	int fd_out_1;
+
+	printf("Dumping Context\n");
+	if (getcontext(&p_context) < 0) {
+		printf("Get Context Failed\n");
+		perror("Error while getting context.");
+		return -1;
+	} else {
+		printf("Got Context %lu\n", sizeof(p_context));
+	}
+	fd_out_1 = open("myckpt1", O_CREAT | O_WRONLY | O_APPEND | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IXUSR);
+	if (write(fd_out_1, &p_context, sizeof(p_context)) < 0) {
+		perror("Error while writing context.");
+		return -1;
+	}
+	printf("Context Dumped Successfully\n");
+	return 0;
+}
--- hw01/libckpt.c
+++ hw01/libckpt.c
@@ -4,23 +4,68 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <ucontext.h>
 
-typedef struct
-{
+const unsigned char IS_MAP = 0x01; // hex for 0000 0001
+const unsigned char IS_CONTEXT = 0x02; // hex for 0000 0010
+
+// byte-size value to hold some combination of the above 8 options
+unsigned char flag = 0; // all flags/options turned off to start
+
+typedef struct {
 	long sAddr;
 	long eAddr;
-	char * cLine;
+	char * perms;
+	unsigned char flags;
+
 } header;
 
-void laodMemHeaders(FILE *fp, header * mem_header_ptrs[]) {
+void parse_mem_header(header * mem_header_ptr, char * line) {
+	const char CONST_SPACE[2] = " ";
+	const char CONST_HYPHEN[2] = "-";
+	char *token;
+	char *add_range;
+	char *flags;
+	char *add_range_token;
+
+//	if(strstr(line,"")!=NULL)
+
+	/*set type to maps*/
+	mem_header_ptr->flags = flag |= IS_MAP;
+
+	/* get the address range*/
+	token = strtok(line, CONST_SPACE);
+	/* copy address range*/
+	add_range = malloc(sizeof(char) * strlen(token));
+	strcpy(add_range, token);
+//	printf("Address Range %s\n", add_range);
+
+	/*get flags*/
+	token = strtok(NULL, CONST_SPACE);
+	/*copy flags*/
+	flags = token;
+	printf("Flags %s\n", flags);
+	mem_header_ptr->perms = flags;
+
+	/*parse address range*/
+	add_range_token = strtok(add_range, CONST_HYPHEN);
+	printf("start %s\n", add_range_token);
+	mem_header_ptr->sAddr = strtol(add_range_token, NULL, 16);
+
+	add_range_token = strtok(NULL, CONST_HYPHEN);
+	printf("end %s\n", add_range_token);
+	mem_header_ptr->eAddr = strtol(add_range_token, NULL, 16);
+
+}
+
+void create_mem_headers(FILE *fp, header * mem_header_ptrs[]) {
 	char * line = NULL;
-	line = (char*)malloc(sizeof(char));
+	line = (char*) malloc(sizeof(char));
 	int i = 0, j = 1, k = 0;
 	char c;
-	while (1)
-	{
-		if ( feof(fp) )
-		{
+	while (1) {
+		if (feof(fp)) {
 			printf("End of file reached\n");
 			free(line); // important step the pointer declared must be made free
 			break;
@@ -29,19 +74,16 @@ void laodMemHeaders(FILE *fp, header * mem_header_ptrs[]) {
 
 		if (c == '\n') {
 			line[i] = '\0'; // at the end append null character to mark end of string
-			mem_header_ptrs[k] = (header*)malloc(sizeof(header));
-			mem_header_ptrs[k]->cLine = line;
-			// strcpy (mem_header_ptrs[k]->cLine, line);
-			mem_header_ptrs[k]->sAddr = 1212;
+			mem_header_ptrs[k] = (header*) malloc(sizeof(header));
+			parse_mem_header(mem_header_ptrs[k], line);
 			printf("End of Line\n");
-			// printf("%s\n", line);
 			line = NULL;
 			i = 0;
 			j = 1;
 			k++;
 			// line
 		} else {
-			line = (char*)realloc(line, j * sizeof(char));
+			line = (char*) realloc(line, j * sizeof(char));
 			// store read character by making pointer point to c
 			line[i] = c;
 			i++;
@@ -51,70 +93,107 @@ void laodMemHeaders(FILE *fp, header * mem_header_ptrs[]) {
 	}
 }
 
-void printMemHeaders(header * mem_header_ptrs[]) {
-	int count = 0;
+void print_mem_headers(header * mem_header_ptr) {
+	printf("Start Add: %ld\n", mem_header_ptr->sAddr);
+	printf("End Add: %ld\n", mem_header_ptr->eAddr);
+	printf("Perms: %s\n", mem_header_ptr->perms);
+	printf("Flags: %u\n", mem_header_ptr->flags);
+
+}
+
+void clearMemHeaders(header * mem_header_ptrs[]) {
+	int index = 0;
 	while (1) {
-		if (mem_header_ptrs[count] == NULL) {
+		if (mem_header_ptrs[index] == NULL) {
 			printf("end of structure\n");
 			break;
 		}
-		printf("%s\n", mem_header_ptrs[count]->cLine);
-		printf("%ld\n", mem_header_ptrs[count]->sAddr);
-		count++;
+		free(mem_header_ptrs[index]);
+		index++;
 	}
 }
 
-void clearMemHeaders( header * mem_header_ptrs[]) {
-	int count = 0;
+void write_mem_headers(int fd, header * mem_header_ptr) {
+	write(fd, (void *) mem_header_ptr, sizeof(header));
+	write(fd, (void *) mem_header_ptr->sAddr,
+			mem_header_ptr->eAddr - mem_header_ptr->sAddr);
+}
+
+void process_mem_headers(int fd_out, header * mem_header_ptrs[]) {
+	int index = 0;
 	while (1) {
-		if (mem_header_ptrs[count] == NULL) {
+		if (mem_header_ptrs[index] == NULL) {
 			printf("end of structure\n");
 			break;
 		}
-		free(mem_header_ptrs[count]);
-		count++;
+		print_mem_headers(mem_header_ptrs[index]);
+		write_mem_headers(fd_out, mem_header_ptrs[index]);
+		index++;
 	}
 }
 
-void sigHandler(int signum)
-{
+int dump_context(int fd_out) {
+	ucontext_t p_context;
+	int fd_out_1;
+
+	printf("Dumping Context\n");
+	if (getcontext(&p_context) < 0) {
+		printf("Get Context Failed\n");
+		perror("Error while getting context.");
+		return -1;
+	} else {
+		printf("Got Context %lu\n", sizeof(p_context));
+	}
+	fd_out_1 = open("myckpt1", O_CREAT | O_WRONLY | O_APPEND | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IXUSR);
+	if (write(fd_out_1, &p_context, sizeof(ucontext_t)) < 0) {
+		perror("Error while writing  context.");
+		return -1;
+	}
+	printf("Context Dumped Successfully\n");
+	return 0;
+}
+void sig_handler(int signum) {
 	printf("Caught signal %d\n", signum);
-	FILE *fp = NULL;
-	header * mem_header_ptrs[40];
-	// header * mem_header_add = malloc(sizeof(header);
+	FILE *fp_in = NULL;
+	int fd_out;
+	header * mem_header_ptrs[100];
 	printf("Reading Memory Maps for the current porcess.\n");
-	fp = fopen("/proc/self/maps", "r");
+	fp_in = fopen("/proc/self/maps", "r");
+	fd_out = open("myckpt", O_CREAT | O_WRONLY | O_APPEND | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IXUSR);
 
-	if (fp == NULL) {
-		perror("Error");
+	if (fp_in == NULL) {
+		perror("Error while opening Input Memory Maps");
 		goto END;
 	}
 
-	laodMemHeaders(fp, mem_header_ptrs);
-	printMemHeaders(mem_header_ptrs);
-	// clearMemHeaders(mem_header_ptrs);
+	if (fd_out < 0) {
+		perror("Error while opening output checkpoint file");
+		goto END;
+	}
 
+	create_mem_headers(fp_in, mem_header_ptrs);
+	process_mem_headers(fd_out, mem_header_ptrs);
+	if (dump_context(fd_out) < 0) {
+		perror("Error while dumping context");
+		goto FILE_CLOSE;
+	}
 
-	// free(line); // important step the pointer declared must be made free
-	// free(line); // important step the pointer declared must be made free
 	printf("Back to Handler\n");
 
-	if (fclose(fp) != 0) {
-		printf("error occured\n");
-		perror("Error");
+	FILE_CLOSE: if (fclose(fp_in) != 0) {
+		perror("Error occurred while closing input file.");
+		goto END;
+	}
+	if (close(fd_out) != 0) {
+		perror("Error occurred while closing output file.");
 		goto END;
 	}
 
-END:
-	printf("End of Signal Handler\n");
+	END: printf("End of Signal Handler\n");
 }
 __attribute__ ((constructor))
 void myconstructor() {
-	signal(SIGUSR2, sigHandler);
+	signal(SIGUSR2, sig_handler);
 }
-
-// char[] getLine(fp) {
-// 	char[]
-
-// }
-
